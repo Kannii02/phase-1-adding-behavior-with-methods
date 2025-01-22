01765ef08f5d8977bdaabec00bf38925ef64c3a8
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-21 21:07:58 -1000

    phase-1-adding-behavior-with-methods

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+// Cat Class
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Dog Class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Bird Class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+
+  const cat = new Cat("Whiskers", "female");
+  console.log(cat.speak()); 
+  
+  const dog = new Dog("Buddy", "male");
+  console.log(dog.speak()); 
+  
+  const maleBird = new Bird("Polly", "male");
+  console.log(maleBird.speak()); 
+  
+  const femaleBird = new Bird("Chirpy", "female");
+  console.log(femaleBird.speak()); 
+  
+  module.exports = { Cat, Dog, Bird };
+  
